e5940e45cfd6c4f243e94308f0510d02f4569899
Dafa Alvin <dafaalvin10@gmail.com>
2025-01-02 09:35:08 +0700

    style: styling footer border

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,25 @@
-// Dark mode functionality
+// Dark Mode
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.body;
 
-// Check for saved user preference
 const darkMode = localStorage.getItem("darkMode");
 
-// Function to enable dark mode
 const enableDarkMode = () => {
   body.classList.add("dark-mode");
   darkModeToggle.innerHTML = '<i class="ph ph-moon"></i>';
   localStorage.setItem("darkMode", "enabled");
 };
 
-// Function to disable dark mode
 const disableDarkMode = () => {
   body.classList.remove("dark-mode");
   darkModeToggle.innerHTML = '<i class="ph ph-sun"></i>';
   localStorage.setItem("darkMode", null);
 };
 
-// Initialize dark mode based on saved preference
 if (darkMode === "enabled") {
   enableDarkMode();
 }
 
-// Toggle dark mode on button click
 darkModeToggle.addEventListener("click", () => {
   const darkMode = localStorage.getItem("darkMode");
   if (darkMode !== "enabled") {
@@ -34,7 +29,7 @@ darkModeToggle.addEventListener("click", () => {
   }
 });
 
-// Existing menu toggle code
+// Responsive Menu Navbar
 const menuIcon = document.getElementById("menu-icon");
 const menuList = document.getElementById("menu-list");
 
